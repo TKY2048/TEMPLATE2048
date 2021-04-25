2d48ceb723fd3d156ac8b3dabccdbf3a052b4688
TKY2048 <tky2048@gmail.com>
2021-04-25 05:14:42 -0500

    Added uikit

---
+++ app/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Initializer: () => React$Node = () => {
+  return <Text>Hola</Text>;
+};
+
+export default Initializer;
--- app/i18n/locales/index.js
+++
@@ -1,5 +0,0 @@
-import es_MX from './es_MX';
-
-export default {
-  es_MX: es_MX,
-};
